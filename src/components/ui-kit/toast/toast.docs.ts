9ff8bbcaccac9f48d3d4eb2d3badd1bbaaf0d66f
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-10-17 10:13:56 -0300

    feat: add toast component

---
+++ src/components/ui-kit/toast/toast.docs.ts
@@ -0,0 +1,5 @@
+export const docs = {
+  title: "Toast",
+  description: "Um componente de toast opinativo para React.",
+  dependencies: ["sonner", " next-themes" , "clsx"], // Dependências reais
+};
